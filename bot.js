e7a161e57d4b5a02e1fce30aa3e05bbd670abe20
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-10 11:20:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,22 @@
-const Discord = require(discord.js);
-cont bot = new Discord.Client();
+const discord = require('discord.js'),
+const bot = new discord.Client(),
+config = require('./config.json'),
+prefix = ".",
+      
+// ===Loading Commands===
+bot.commands = new discord.Collection()
 
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+// ===Done Loading commands===
+
+bot.on('ready', () => {
+  
 
 
 bot.login(process.env.TOKEN)
