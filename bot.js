76515ff2757fa5cbd9447b8e4c7c3adf3d554cdf
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-10 11:39:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,6 @@ const bot = new discord.Client(),
 config = require('./config.json'),
 prefix = ".",
       
-// ===Loading Commands===
-bot.commands = new discord.Collection()
-
-require('fs').readdir("./commands/", (err, files) => {
-  console.log("Loading commands...");
-  if (err) return console.log(`Command loading failed!`);
-  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
-    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
-  });
-});
-// ===Done Loading commands===
 
 bot.on('ready', () => {
   bot.user.setActivity('HentaiGod being touched as a child', {type: "WATCHING"})
